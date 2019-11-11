02bbb7233781adbf532a456b93fc0f9194859363
LabMemEL <yunxi.li@hotmail.com>
2019-11-10 17:49:39 -0800

    Spark Job: Aggregation and Join
    More work on GDELT data like aggregating and joining GDELT is moved from DB to the Spark Dataframe job, leaving indexing the only work in MySQL.
    Object structure is reorganized to allow easier batch size changes, which is expected for future automated tasking.

--- spark_workspace/spark_gdelt/src/main/scala/SimpleApp.scala
+++ spark_workspace/spark_gdelt/src/main/scala/SimpleApp.scala
@@ -1,15 +1,17 @@
 // package gdelt.spark
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SQLContext
+// import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, concat_ws, lit}
-import scala.io.Source
+import org.apache.spark.sql.functions.{col, concat_ws, lit, collect_list, max}
+
+// import scala.io.Source
 import java.util.Properties
 import com.typesafe.config.ConfigFactory
+
 // import com.amazonaws.services.s3.AmazonS3Client
 // import com.amazonaws.services.s3.model.GetObjectRequest
-import org.apache.spark.{SparkContext, SparkConf}
-import java.sql.{DriverManager, Connection}
+// import org.apache.spark.{SparkContext, SparkConf}
+// import java.sql.{DriverManager, Connection}
 
 object SimpleApp {
   val jdbc_path = ConfigFactory.load().getConfig("main").getString("jdbc")
@@ -18,65 +20,22 @@ object SimpleApp {
   val username = ConfigFactory.load().getConfig("main").getString("user")
   val password = ConfigFactory.load().getConfig("main").getString("passwd")
 
-  def create_tb_events(connection: Connection, date: String){
-      val statement = connection.createStatement
-      val update = "CREATE TABLE IF NOT EXISTS TB_" + date + """ (
-                    EID INT PRIMARY KEY,
-                    Date DATETIME NOT NULL,
-                    K_words TEXT,
-                    N_Mentions INT,
-                    N_Sources INT,
-                    N_Discs INT,
-                    URL TEXT NOT NULL,
-                    INDEX(URL(10))
-                    )  ENGINE=INNODB;"""
-      val rs = statement.executeUpdate(update)
+  
+  def load_S3_DF(spark: SparkSession, isEvent: Boolean, batch_unit : String): DataFrame = {
+    //load either events or mentions data from S3 with yearly/monthly/daily/15-min batches
+    val (sourcetype, extention) =
+      if (isEvent) ("events/", "*.export.csv")
+      else ("mentions/","*.mentions.csv")
+
+    val loadedDF = spark.read.format("csv")
+    .option("sep", "\t")
+    .option("mode", "DROPMALFORMED")
+    .load("s3a://insightdatarichard/v2/" + sourcetype + batch_unit + "18230000" + extention) //fileName; "s3://" triggers jets3t exception
+    loadedDF
   }
 
-  def create_tb_mentions(connection: Connection, date: String){
-    val statement = connection.createStatement
-    val update = "CREATE TABLE IF NOT EXISTS MT_" + date + """ (
-                  EID INT NOT NULL,
-                  Init_Date DATETIME,
-                  Curr_Date DATETIME,
-                  Sc_Name TEXT,
-                  Sc_URL TEXT,
-                  Conf INT,
-                  INDEX(EID)
-                  )  ENGINE=INNODB;"""
-    val rs = statement.executeUpdate(update)
-  }
-
-  def create_tb_finals(connection: Connection, date: String){
-    // val limit_st = "SET GROUP_concat_max_len=15000"
-    val statement = connection.createStatement
-    val concate_st = "CREATE TABLE IF NOT EXISTS EX_" + date + """ (INDEX(EID))
-                      SELECT mt.EID AS EID, COUNT('tb.EID') AS S_nums,
-                      MAX(timestampdiff(minute, mt.Init_Date, mt.Curr_date)) AS max_span,
-                      GROUP_CONCAT(mt.Sc_Name SEPARATOR '\t') AS S_names,
-                      GROUP_CONCAT(mt.Sc_URL SEPARATOR '\t') AS S_urls
-                      FROM MT_""" + date + " mt, TB_" + date + """ tb
-                      WHERE tb.EID = mt.EID
-                      GROUP BY tb.EID"""
-    statement.executeUpdate(concate_st)
-
-    val join_st = "CREATE TABLE IF NOT EXISTS RS_" + date + """ (INDEX(URL(10),Date))
-                  SELECT tb.EID,
-                  tb.Date as Date,
-                  tb.K_words,
-                  tb.N_Mentions,
-                  tb.N_Discs,
-                  tb.URL as URL,
-                  ex.S_nums,
-                  ex.max_span,
-                  ex.S_names,
-                  ex.S_urls
-                  FROM TB_""" + date + " tb LEFT JOIN EX_" + date + " ex USING(EID)"
-    statement.executeUpdate(join_st)
-  }
-
-  def db_write(records_df: DataFrame, tablename: String) {  //update(records_df: DataFrame)
-
+  def write_DF_DB(records_df: DataFrame, tablename: String) {
+    // load DB configurations
     val connectionProperties = new Properties()
     connectionProperties.put("user", username)
     connectionProperties.put("password", password)
@@ -87,51 +46,50 @@ object SimpleApp {
           .jdbc(jdbc_path, "gdeltDB."+ tablename, connectionProperties)
   }
 
+  def aggregate_mentions(spark: SparkSession, month: String): DataFrame = {
+    // load mentions data and aggregate to unique event record
+    val ex_key_cols = Seq("EID", "Init_Date", "Curr_Date", "Sc_URL")
 
-  def filter_mentions(connection: Connection, spark: SparkSession){
+    val mentionsDF = load_S3_DF(spark, isEvent = false, month)
+    val pre_agg_DF = mentionsDF
+      .select("_c0","_c1","_c2","_c5")
+      .toDF(ex_key_cols: _*)
+      .withColumn("Days_Passed", ((col("Curr_date").cast("long") - col("Init_Date").cast("long"))/86400)
+      .cast("Int"))
+      .select("EID", "Days_Passed", "Sc_URL")
 
-    val fileNames = Source.fromFile("mentionslist.out").getLines.toList.drop(1700)take(1000)
-    val months = fileNames.map(x => x.split("/")(2).substring(0,6)).distinct
-    for (month <- months) {
-      create_tb_mentions(connection, month) //consider if check to run only monthly
-      val full_df = spark.read.format("csv")
-          .option("sep", "\t")
-          // .option("header", "true") //first line in file has headers
-          .option("mode", "DROPMALFORMED")
-          .load("s3a://insightdatarichard/v2/mentions/"+ month + "*.csv") //s3 triggers jets3t exception
-      // gdelt_full_df.show()
-      val rename = Seq("EID", "Init_Date", "Curr_Date", "Sc_Name", "Sc_URL", "Conf")
-      val main_DF  = full_df.select("_c0","_c1","_c2","_c4","_c5","_c11")
-      val renamed_df = main_DF.toDF(rename: _*)
-      // print(renamed_df.show(2))
-      db_write(renamed_df, "MT_" + month)
-    }
-    for (month <-months){
-      create_tb_finals(connection, month)
-    }
+    val aggregatedDF = pre_agg_DF
+      .groupBy("EID")
+      .agg(collect_list("Sc_URL") as "URLs", max(col("Days_Passed")) as "Coverage")
+      .withColumn("URLs", concat_ws("\t", col("URLs")))
+    aggregatedDF
   }
 
-  def filter_events(connection: Connection, spark: SparkSession){
+  def join_events(spark: SparkSession, exentionsDF: DataFrame, month: String): DataFrame = {
+    // load unique events data and left join with mentions data
+    val main_cols = Seq("EID", "Date", "act1", "act2", "N_Mentions", "N_Sources", "N_Discs", "URL")
+    
+    val eventsDF = load_S3_DF(spark, isEvent = true, month)
+    val kwordsDF = eventsDF
+      .select("_c0","_c1","_c6","_c16","_c31","_c32","_c33","_c60")
+      .toDF(main_cols: _*)
+      .withColumn("K_words", concat_ws(",", col("act1"), col("act2")))
+      .drop("act1", "act2")
+    val final_DF = kwordsDF
+      .join(exentionsDF, Seq("EID"), "left_outer")
+    final_DF
+  }
 
-    val fileNames = Source.fromFile("eventlist.out").getLines.toList.drop(35000).take(1000)
-    val months = fileNames.map(x => x.split("/")(2).substring(0,6)).distinct //(0, 6) for montly, (0,8) for daily
-    for (month <- months) {
-      create_tb_events(connection, month)
-      val batch_df = spark.read.format("csv")
-          .option("sep", "\t")
-          // .option("header", "true") //first line in file has headers
-          .option("mode", "DROPMALFORMED")
-          .load("s3a://insightdatarichard/v2/events/" + month + "*.csv") //fileName; "s3://" triggers jets3t exception
-      // gdelt_full_df.show()
-
-      val rename = Seq("EID", "Date", "act1", "act2", "N_Mentions", "N_Sources", "N_Discs", "URL")
-      val main_DF  = batch_df.select("_c0","_c1","_c6","_c16","_c31","_c32","_c33","_c60")
-      val temp_df = main_DF.toDF(rename: _*)
-      val final_df = temp_df.withColumn("K_words", concat_ws(",", col("act1"), col("act2")))
-                            .drop("act1", "act2")
-      // print(final_df.show(2))
-      db_write(final_df, "TB_" + month)
-    }
+  def batch_monthly(spark: SparkSession) {
+    // val year = Array("2015, 2016, 2017")
+    // val months = Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+    val year = "2015"
+    val months = Array("02")
+      for (month <- months) {
+        val mentionsDF = aggregate_mentions(spark, year + month)
+        val expandedDF = join_events(spark, mentionsDF, year + month)
+        write_DF_DB(expandedDF, "FN_" + month)
+      }
   }
 
   def main(args: Array[String]) {
@@ -140,33 +98,12 @@ object SimpleApp {
         .builder()
         .appName("GDELT_process")
         .getOrCreate()
-
-    // val url = "jdbc:mysql://ec2-35-165-132-83.us-west-2.compute.amazonaws.com:3306/gdeltDB"
-    // val driver = "com.mysql.cj.jdbc.Driver"
-    // val username = "sparkMS"
-    // val password = "test"
+    
     try {
-      Class.forName(driver)
-      val dbConnection = DriverManager.getConnection(url, username, password)
-      filter_events(dbConnection, spark)
-      // filter_mentions(dbConnection, spark)
-
-      dbConnection.close
+      batch_monthly(spark)
     } catch {
       case e: Exception => e.printStackTrace
     }
-
-    // val fileName = "v2/events/20150218230000.export.csv"
-
-
-    // val linesRdd = sc.textFile("s3://insightdatarichard/"+ fileName)
-    // val linesMap = linesRdd.map( line => line.split("\t") )
-    // val lineRdd = linesMap.map( fields => {
-    //   val eventID = fields(0)
-    //   val date = fields(1)
-    // } )
-    //collect() fetches the entire RDD to a single machine
-    // lineRdd.foreach( { line => println( s"{ $eventID, $date }" ) } )
 
     spark.stop()
   }
